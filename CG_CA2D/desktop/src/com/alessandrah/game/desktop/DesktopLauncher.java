e363a83babdd0f49740f151dd7a81a31cc0643fa
AlessandraH <alehairi@gmail.com>
2017-06-14 13:12:06 -0300

    adicionando projeto libgdx

---
+++ CG_CA2D/desktop/src/com/alessandrah/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.alessandrah.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.alessandrah.game.CA2D;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CA2D(), config);
+	}
+}
