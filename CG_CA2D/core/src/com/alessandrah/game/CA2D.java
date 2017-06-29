66779a6000e1d4448fa6322e2a196ec0b79e083e
AlessandraH <alehairi@gmail.com>
2017-06-29 12:39:40 -0300

    finalizando detalhes

--- CG_CA2D/core/src/com/alessandrah/game/CA2D.java
+++ CG_CA2D/core/src/com/alessandrah/game/CA2D.java
@@ -22,8 +22,8 @@ import java.util.ArrayList;
 public class CA2D extends ApplicationAdapter implements ApplicationListener {
     SpriteBatch batch;
     OrthographicCamera camera;
-    //Texture img;
-    //Sprite sprite;
+    Texture img;
+    Sprite sprite;
     Stage stage;
     ArrayList<Figura> objetos = new ArrayList<Figura>();
     ShapeRenderer renderizador;
@@ -49,8 +49,8 @@ public class CA2D extends ApplicationAdapter implements ApplicationListener {
         camera.setToOrtho(false);
         camera.update();
 
-        //img = new Texture(Gdx.files.internal("assets/ca2d.png"));
-        //sprite = new Sprite(img);
+        img = new Texture(Gdx.files.internal("ca2d.png"));
+        sprite = new Sprite(img);
 
         batch = new SpriteBatch();
 
@@ -81,7 +81,7 @@ public class CA2D extends ApplicationAdapter implements ApplicationListener {
         stage.addListener(new ClickListener(Input.Buttons.LEFT) {
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                System.out.println("Você clicou em [" + x + "][" + y + "]");
+                //System.out.println("Você clicou em [" + x + "][" + y + "]");
 
                 if (desenhando) {
                     figura.setCoordenadas(clique, x, y);
@@ -136,17 +136,17 @@ public class CA2D extends ApplicationAdapter implements ApplicationListener {
         renderizador.end();
 
         batch.begin();
-        //sprite.draw(batch,0.3f);
+        sprite.draw(batch, 0.3f);
         verificaTeclaPressionada();
         if (terminal)
-            fonte.draw(batch, terminalmsg + text, 0, 700);
+            fonte.draw(batch, terminalmsg + text, 5, 750);
 
         batch.end();
     }
 
     @Override
     public void dispose() {
-        //img.dispose();
+        img.dispose();
         batch.dispose();
     }
 
@@ -185,36 +185,50 @@ public class CA2D extends ApplicationAdapter implements ApplicationListener {
         if (!terminal && Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
             terminal = true;
             terminalmsg = exibeInstrucoes();
-            fonte.draw(batch, terminalmsg, 0, 700);
+            fonte.draw(batch, terminalmsg, 5, 750);
         } else if (terminal && Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
             if (deletando) {
-                indice = Integer.parseInt(text.trim()) - 1;
+                if (text.length()==1) {
+                    indice = 0;
+                } else {
+                    indice = Integer.parseInt(text.trim()) - 1;
+                }
                 objetos.remove(indice);
                 inicializaVariaveis();
             } else if (pegandoCoordenadas) {
                 if (rotacionando) {
-                    angulo = Float.parseFloat(text.trim());
+                    if (text.length()==1) {
+                        angulo = 45;
+                    } else {
+                        angulo = Float.parseFloat(text.trim());
+                    }
                     terminal = false;
                     pegandoCoordenadas = false;
                 } else if (mudandoEscala) {
-                    s = Float.parseFloat(text.trim());
+                    if (text.length()==1) {
+                        s = 2;
+                    } else {
+                        s = Float.parseFloat(text.trim());
+                    }
                     terminal = false;
                     pegandoCoordenadas = false;
                 }
             } else if (rotacionando || transladando || mudandoEscala) {
-                indice = Integer.parseInt(text.trim()) - 1;
+                if (text.length()==1) {
+                    indice = 0;
+                } else {
+                    indice = Integer.parseInt(text.trim()) - 1;
+                }
                 figuraTransformar = new Figura();
                 figuraTransformar = objetos.get(indice);
                 pegandoCoordenadas = true;
                 text = "";
                 if (rotacionando) {
-                    //pegandoCoordenadas = true;
                     terminalmsg = "Digite o angulo de rotação: ";
-                    fonte.draw(batch, terminalmsg, 0, 700);
+                    fonte.draw(batch, terminalmsg, 5, 750);
                 } else if (mudandoEscala) {
-                    //pegandoCoordenadas = true;
                     terminalmsg = "Digite o valor de escala: ";
-                    fonte.draw(batch, terminalmsg, 0, 700);
+                    fonte.draw(batch, terminalmsg, 5, 750);
                 }
             } else {
                 terminal = false;
@@ -224,57 +238,70 @@ public class CA2D extends ApplicationAdapter implements ApplicationListener {
         if (!(desenhando || deletando || rotacionando || mudandoEscala || transladando)) {
             if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_1)) {
                 terminal = true;
-                System.out.println("Você pressionou 1");
+                //System.out.println("Você pressionou 1");
+                //terminalmsg = "Clique em 2 pontos para desenhar o círculo";
+                //fonte.draw(batch, terminalmsg, 5, 750);
                 desenhando = true;
                 figura = new Circulo(1);
             } else if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_2)) {
-                System.out.println("Você pressionou 2");
+                //System.out.println("Você pressionou 2");
+                //terminalmsg = "Clique em 2 pontos para desenhar a reta";
+                //fonte.draw(batch, terminalmsg, 5, 750);
                 desenhando = true;
                 figura = new Figura(2);
             } else if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_3)) {
-                System.out.println("Você pressionou 3");
+                //System.out.println("Você pressionou 3");
+                //terminalmsg = "Clique em 3 pontos para desenhar o triângulo";
+                //fonte.draw(batch, terminalmsg, 5, 750);
                 desenhando = true;
                 figura = new Figura(3);
             } else if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_4)) {
-                System.out.println("Você pressionou 4");
+                //System.out.println("Você pressionou 4");
+                //terminalmsg = "Clique em 4 pontos para desenhar o quadrilátero";
+                //fonte.draw(batch, terminalmsg, 5, 750);
                 desenhando = true;
                 figura = new Figura(4);
             } else if (Gdx.input.isKeyJustPressed(Input.Keys.BACKSPACE)) {
                 if (objetos.size() > 0) {
-                    System.out.println("Você pressionou backspace");
+                    //System.out.println("Você pressionou backspace");
                     deletando = true;
+                    terminalmsg = "Apagar figura\n";
                     exibeTerminal();
+                    fonte.draw(batch, terminalmsg, 5, 750);
                 } else {
-                    System.out.println("Não há objetos");
+                    //System.out.println("Não há objetos");
                 }
             } else if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_0)) {
-                System.out.println("Você pressionou delete");
+                //System.out.println("Você pressionou delete");
                 deletando = true;
                 objetos.clear();
                 inicializaVariaveis();
             } else if (Gdx.input.isKeyJustPressed(Input.Keys.F1)) {
-                System.out.println("Você escolheu mudar de escala");
+                //System.out.println("Você escolheu mudar de escala");
                 mudandoEscala = true;
+                terminalmsg = "Mudar de escala\n";
                 exibeTerminal();
             } else if (Gdx.input.isKeyJustPressed(Input.Keys.F2)) {
-                System.out.println("Você escolheu rotacionar");
+                //System.out.println("Você escolheu rotacionar");
                 rotacionando = true;
+                terminalmsg = "Rotacionar\n";
                 exibeTerminal();
             } else if (Gdx.input.isKeyJustPressed(Input.Keys.F3)) {
-                System.out.println("Você escolheu transladar");
+                //System.out.println("Você escolheu transladar");
                 transladando = true;
+                terminalmsg = "Transladar\n";
                 exibeTerminal();
             }
         }
         if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
-            System.out.println("Você pressionou Esc");
+            //System.out.println("Você pressionou Esc");
             inicializaVariaveis();
         }
     }
 
     public void exibeTerminal() {
-        terminalmsg = exibeListaObjetos();
-        fonte.draw(batch, terminalmsg, 0, 700);
+        terminalmsg += exibeListaObjetos();
+        fonte.draw(batch, terminalmsg, 5, 750);
     }
 
     public String exibeListaObjetos() {
@@ -370,25 +397,30 @@ public class CA2D extends ApplicationAdapter implements ApplicationListener {
         String instrucoes = "";
         instrucoes += "CA2D - Instruções de uso\n\n";
         instrucoes += "Desenho:\n";
-        instrucoes += "- Para desenhar um CÍRCULO, pressione <1> e, em seguida, clique em dois pontos \n\t(o primeiro clique determinará o centro do círculo, e o segundo servirá para o cálculo do raio);\n";
-        instrucoes += "- Para desenhar uma RETA, pressione <2> e, em seguida, clique em dois pontos, \n\tque determinarão o início e o fim da reta;\n";
-        instrucoes += "- Para desenhar um TRIÂNGULO, pressione <3> e, em seguida, clique em três pontos, \n\tque determinarão os vértices do triângulo;\n";
-        instrucoes += "- Para desenhar um QUADRILÁTERO, pressione <4> e, em seguida, clique em quatro pontos, \n\tque determinarão os vértices do quadrilátero.\n\n";
+        instrucoes += "- Para desenhar um CÍRCULO, pressione <1> e, em seguida, clique em dois pontos (o primeiro clique determinará o centro do círculo, e o segundo servirá para o cálculo do raio);\n";
+        instrucoes += "- Para desenhar uma RETA, pressione <2> e, em seguida, clique em dois pontos, que determinarão o início e o fim da reta;\n";
+        instrucoes += "- Para desenhar um TRIÂNGULO, pressione <3> e, em seguida, clique em três pontos, que determinarão os vértices do triângulo;\n";
+        instrucoes += "- Para desenhar um QUADRILÁTERO, pressione <4> e, em seguida, clique em quatro pontos, que determinarão os vértices do quadrilátero.\n\n";
         instrucoes += "Transformações 2D:\n";
         instrucoes += "- Para realizar uma MUDANÇA DE ESCALA, pressione <F1>. " +
-                "\n\tEm seguida, digite o número correspondente, da lista, da figura que deseja realizar a transformação. " +
-                "\n\tDepois, insira o valor de escala. Por fim, clique em um ponto que servirá de referência para a transformação;\n";
+                "Em seguida, digite o número correspondente, da lista, da figura que deseja realizar a transformação. " +
+                "\nDepois, insira o valor de escala. Por fim, clique em um ponto que servirá de referência para a transformação;\n";
         instrucoes += "- Para realizar uma ROTAÇÃO, pressione <F2>. " +
-                "\n\tEm seguida, digite o número correspondente, da lista, da figura que deseja realizar a transformação." +
-                "\n\tDepois, digite o ângulo, em graus. Por fim, clique em um ponto que servirá de referência para a transformação;\n";
+                "Em seguida, digite o número correspondente, da lista, da figura que deseja realizar a transformação." +
+                "\nDepois, digite o ângulo, em graus. Por fim, clique em um ponto que servirá de referência para a transformação;\n";
         instrucoes += "- Para realizar uma TRANSLAÇÃO, pressione <F3>. " +
-                "\n\tEm seguida, digite o número correspondente, da lista, da figura que deseja realizar a transformação. " +
-                "\n\tPor fim, clique em um ponto que servirá de referência para a transformação.\n\n";
+                "Em seguida, digite o número correspondente, da lista, da figura que deseja realizar a transformação. " +
+                "\nPor fim, clique em um ponto que servirá de referência para a transformação.\n\n";
         instrucoes += "Outras operações:\n";
         instrucoes += "- Para APAGAR APENAS UMA FIGURA, pressione <backspace>. " +
-                "\n\tEm seguida, digite o número correspondente, da lista, da figura que deseja realizar apagar;\n";
+                "Em seguida, digite o número correspondente, da lista, da figura que deseja realizar apagar;\n";
         instrucoes += "- Para APAGAR TODAS AS FIGURAS (CLEAR), pressione <0>;\n";
-        instrucoes += "- Para SAIR, pressione <ALT+F4>.";
+        instrucoes += "- Para SAIR, pressione <ALT+F4>.\n\n";
+        instrucoes += "Observações:\n";
+        instrucoes += "- A translação é feita em relação ao primeiro vértice (primeiro ponto clicado) no momento da criação da figura;\n";
+        instrucoes += "- Se a figura a ser selecionada não for especificada, a primeira figura será selecionada;\n";
+        instrucoes += "- Se o ângulo da rotação não for especificado, o ângulo será 45 graus;\n";
+        instrucoes += "- Se a escala da mudança de escala não for especificada, a escala será 2.";
         return instrucoes;
     }
 }
